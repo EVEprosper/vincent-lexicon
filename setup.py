033c6a40d8d0a30fd09347b82e20e86b1f79dd3a
lockefox <locke.renard@gmail.com>
2018-07-07 08:14:56 -0700

    initial commit -- drivendata/cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='NLP sentiment analysis for corperate-speak',
+    author='John Purcell',
+    license='MIT',
+)
